3a1037210ad4c63f6a902608c64062f4bc5c4a33
Tim <tim.bathgate@gmail.com>
2017-10-25 15:52:25 +1300

    Adds DeckView UI

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,23 +1,75 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { getDeck } from '../util/api';
 
 const styles = {
   container: {
     flex: 1,
-    height: 100,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    marginTop: 100,
+    marginBottom: 100
+  },
+  deckTitle: {
+    fontSize: 24
+  },
+  cardCount: {
+    fontSize: 20
+  },
+  buttonContainer: {
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    height: 44,
+    backgroundColor: 'green',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5
   }
 }
 
+/*
+Add the relevant data to the UI, retrieved from api.getDeck(title).
+This will be a Text element with the deck title, a Text element with the number
+of cards in the deck*, a TouchableOpacity with the option to start a quiz, and
+a TouchableOpacity with the option to add a new question to the deck
+*/
 export default class DeckView extends Component {
+
+  state = {
+    title: '',
+    questions: []
+  }
+
+  componentDidMount(){
+    let { title } = this.props.navigation.state.params;
+
+    getDeck(title).then( deck => {
+      this.setState({ title, ...deck })
+    })
+  }
 
   render(){
 
+    let { title, questions } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Deck View</Text>
-        <Text>A deck view</Text>
+        <Text style={styles.deckTitle}>{title}</Text>
+        <Text>{questions.length} cards</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{color: 'white'}}>Add Question</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{color: 'white'}}>Start Quiz</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
--- components/NewDeckView.js
+++ components/NewDeckView.js
@@ -36,8 +36,6 @@ export default class NewDeckView extends Component {
     tabBarLabel: "Add Deck"
   });
 
-
-
   state = {
     title: ''
   }
